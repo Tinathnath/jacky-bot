649a05023f909e010ab4a86ee973b52d478b8ba2
Nathan AVENEL <navenel@circum.fr>
2017-11-13 14:04:48 +0100

    Add front end server

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 console.log('Starting Jacky...');
 
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/src/static'));
+app.set('views', path.join(__dirname, '/src/static/views'));
+
 const Config = require('./config')
 const Discord = require('discord.js')
 const Jacky = require('./src/Jacky');
 
 const client = new Discord.Client();
 const bot = new Jacky(client);
+
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+app.listen(port, () => {
+    console.log('Front-end server started. Listening on http://localhost:' + port);
+});
 
 client.on('ready', () => {
     console.log("Connected to Discord");
